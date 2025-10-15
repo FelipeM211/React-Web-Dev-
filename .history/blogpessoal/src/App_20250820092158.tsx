30e3a8caca26877f9f077d2476ca371b3193cfba
Felipe Balbino Murad <felipebmurad@gmail.com>
2025-10-15 09:11:10 -0300

    .

--- .history/blogpessoal/src/App_20250820092158.tsx
+++
@@ -1,5 +0,0 @@
-import Home from './pages/home/Home'
-
-function App() {
-  return 
-}
--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
+import Footer from './components/footer/footer'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
+    <NavBar />
     <Home />
+    <Footer />
+    </BrowserRouter>
+    </>
+    
   )
 }
 
